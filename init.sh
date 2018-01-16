cf2a41c696611fa8593f1246d8089649919dd4ce
henices <zhouzhenster@gmail.com>
2018-01-16 13:51:50 +0800

    do some inititial, better performance

---
+++ init.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+echo core >/proc/sys/kernel/core_pattern
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo 1 >/proc/sys/kernel/sched_child_runs_first
+echo 1 >/proc/sys/kernel/sched_autogroup_enabled
+
+cd /sys/devices/system/cpu
+echo performance | tee cpu*/cpufreq/scaling_governor
