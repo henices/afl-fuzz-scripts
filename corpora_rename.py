302bfd66f2d2ca99ba0e20aa20546ef1bb4f48b3
henices <zhouzhenster@gmail.com>
2018-05-18 09:53:01 +0800

    add support afl working directory

--- corpora_rename.py
+++ corpora_rename.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 #coding: utf-8
 
+# code by henices
+
 import sys
 import os
 import os.path
@@ -8,9 +10,16 @@ import argparse
 from shutil import copyfile
 
 def _format(idx):
+    """ Format to afl corpus
+    """
     length = len(str(idx))
     fix_length = 6 - length
     return 'id:' + '0' * fix_length + str(idx) + ','
+
+def is_sync_file(s):
+    if ',sync:' in s:
+        return True
+    return False
 
 if __name__ == '__main__':
 
@@ -19,23 +28,29 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input',required=True, help='input directory')
     parser.add_argument('-o', '--output',required=True, help='output directory')
     parser.add_argument('-n', '--number', type=int, required=False, default=0,
-            help='first id')
+                        help='first id')
+    parser.add_argument('--afl', required=False, action='store_true',
+                        help='afl working directory')
     args = parser.parse_args()
 
     in_dir = args.input
     out_dir = args.output
     idx = args.number
+    is_afl = args.afl
 
-    os.mkdir(out_dir)
+    try:
+        os.makedirs(out_dir, exist_ok=True)
+    except Exception as e:
+        print(str(e))
 
     for (root, dirs, files) in os.walk(in_dir):
         for sample in files:
+            if is_afl and is_sync_file(sample):
+                continue
+
             src_path = os.path.join(root, sample)
             afl_testcase = _format(idx) + sample
             target_path = os.path.join(out_dir, afl_testcase)
-            try:
-                copyfile(src_path, target_path)
-            except Exception as e:
-                print(e)
+            copyfile(src_path, target_path)
 
             idx += 1
