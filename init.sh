1fb4aa0dba929d9f75f961e549a5082db0a06e6b
henices <henices@163.com>
2021-02-03 13:42:29 +0800

    linux performance option

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-echo core >/proc/sys/kernel/core_pattern
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
 echo 1 >/proc/sys/kernel/sched_child_runs_first
 echo 1 >/proc/sys/kernel/sched_autogroup_enabled
-
-cd /sys/devices/system/cpu
-echo performance | tee cpu*/cpufreq/scaling_governor
-
-
 echo 1 > /proc/sys/vm/overcommit_memory
+
+echo core >/proc/sys/kernel/core_pattern
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+test -e /sys/devices/system/cpu/cpufreq/scaling_governor && echo performance | tee /sys/devices/system/cpu/cpufreq/scaling_governor
+test -e /sys/devices/system/cpu/cpufreq/policy0/scaling_governor && echo performance | tee /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
+test -e /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor && echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+test -e /sys/devices/system/cpu/cpufreq/boost && echo 1 > /sys/devices/system/cpu/cpufreq/boost
